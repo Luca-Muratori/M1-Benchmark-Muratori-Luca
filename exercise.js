57545530549dfbe7a26fe70b6b2276f1f2d5073a
Lucai994 <luca.muratori.94@hotmail.it>
2021-12-10 12:28:15 +0100

    solved other exercise

--- exercise.js
+++ exercise.js
@@ -32,3 +32,67 @@ newLine('exercise 25')
 console.log(arrayString24)
 
 newLine('exercise 26')
+//26) Create an array with 100 random numbers in it
+let arrayRandom26=[]
+for(i=0; i<100; i++){
+    arrayRandom26[i]=Math.floor(Math.random()*100)+1
+}
+
+console.log(arrayRandom26)
+
+newLine('exercise 27')
+//27) Write a function to get the maximum and minimum values from the previously created array
+Array.prototype.max = function() {
+    return Math.max.apply(null, this);
+  };
+  
+  Array.prototype.min = function() {
+    return Math.min.apply(null, this);
+  };
+  
+  
+  console.log("Max value is: "+arrayRandom26.max()+"\nMin value is: "+ arrayRandom26.min());
+
+
+  newLine('exercise 28')
+  //28) Create an array of arrays, in which every array has 10 random numbers
+  let activities = [
+    ['Work', 9],
+    ['Eat', 1],
+    ['Commute', 2],
+    ['Play Game', 1],
+    ['Sleep', 7]
+];
+
+newLine('exercise 29')
+//9) Create a function that gets 2 arrays as parameters and returns the longest one
+const exercise29=function(arr1, arr2){
+    return arr1.length>arr2.length?console.log(`arr1 is longer than arr2`):
+                                   console.log(`arr2 is longer than arr`)
+}
+  
+let arr1=activities
+let arr2=[1, 2, 3, 4, 5, 6, 7, 8]
+
+exercise29(activities, arr2)
+
+newLine('exercise 30')
+//30) Create a function that gets 2 arrays of numbers as parameters and returns the one with the higher sum of values
+const exercise30=function(arrNum1, arrNum2){
+    let sum1=0
+    for(let i=0; i<arrNum1.length; i++){
+        sum1+=arrNum1[i]
+        return sum1
+    }
+    let sum2=0
+    for (i=0; i<arrNum2.length; i++){
+        sum2+=arrNum1[i]
+        return sum2
+    }
+
+    sum1>sum2?console.log(`${arrNum1} have the higher sum of values`):(`${arrNum2} have the higher sum of values`)
+}
+
+let arrNum1=[1, 2, 3, 4, 5]
+let arrNum2=[1, 2, 3]
+console.log(exercise30(arrNum1, arrNum2))
